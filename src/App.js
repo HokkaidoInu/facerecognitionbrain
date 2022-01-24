8878dcf1db81250d1a95283e9887fec78bfd3a7c
HokkaidoInu <pepegavibe@gmail.com>
2022-01-24 23:08:23 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import Particles from "react-tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+    <Particles 
+    params={particlesOptions} />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
